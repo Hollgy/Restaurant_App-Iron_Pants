34557389b79f58bcefa637f12de24a0b588e6607
KG <karlgustav.ek@hotmail.com>
2023-04-05 14:22:38 +0200

    font added, Icons 100% done and @media added

--- src/components/Linkicons.jsx
+++ src/components/Linkicons.jsx
@@ -10,22 +10,34 @@ function Linkicons() {
         <section className='Buttongroup'>
             <div>
                 <a href=''>
-                    <img className='linkicon1' src={ButtonImage1} alt="Button 1" />
+                    <div className='figure linkicon1'>
+                        <img className='icon' src={ButtonImage1} alt="Button 1" />
+                        <p className='icontext'>Meny</p>
+                    </div>
                 </a>
             </div>
             <div>
                 <a href=''>
-                    <img className='linkicon2' src={ButtonImage2} alt="Button 2" />
+                    <div className='figure linkicon2'>
+                        <img className='icon' src={ButtonImage2} alt="Button 2" />
+                        <p className='icontext'>Drycker</p>
+                    </div>
                 </a>
             </div>
             <div>
                 <a href=''>
-                    <img className='linkicon3' src={ButtonImage3} alt="Button 3" />
+                    <div className='figure linkicon3'>
+                        <img className='icon' src={ButtonImage3} alt="Button 3" />
+                        <p className='icontext'>Event</p>
+                    </div>
                 </a>
             </div>
             <div>
                 <a href=''>
-                    <img className='linkicon4' src={ButtonImage4} alt="Button 4" />
+                    <div className='figure linkicon4'>
+                        <img className='icon' src={ButtonImage4} alt="Button 4" />
+                        <p className='icontext'>Hitta Hit!</p>
+                    </div>
                 </a>
             </div>
         </section>
