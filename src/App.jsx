b0c8f657b973c2f3add1e9be156083304b0e661d
KG <karlgustav.ek@hotmail.com>
2023-04-06 09:40:36 +0200

    readded imports

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,16 @@ import Header from './components/Header.jsx'
 import Footer from './components/Footer.jsx'
 import Body from './components/Body.jsx'
 import Menu from './components/Menu.jsx'
+import Linkicons from './components/Linkicons'
+import Background from './components/background'
 import './App.css'
 import './components/Header.css'
 import './components/Footer.css'
 import './components/Body.css'
 import './components/Navbar.css'
 import './components/Menu.css'
+import './components/Linkicons.css'
+import './components/Background.css'
 
 function App() {
     const [menu, setMenu] = useState(menuList)
@@ -21,6 +25,8 @@ function App() {
             <Header />
             <Body />
             <Menu menu={menu} setMenu={setMenu} />
+            <Linkicons />
+            <Background />
             <Footer />
 
         </div>
