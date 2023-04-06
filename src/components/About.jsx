92002dab84c11b11442742751849d88ca7e5d7ba
Abbe <abdullah.abou.shihab@icloud.com>
2023-04-06 13:49:23 +0200

    testar  om oss sidan

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
+import image from '/public/images/about_backdrop.jpg';
+import './AboutUs.css';
 
-const About = () =>{
-    return (
-        <div>
-        <h1>Helo</h1>
-        </div>
-    )
+function AboutUs() {
+  return (
+    <div className="about">
+      <img src={image} alt="about us" />
+      <p>Our company was founded in 2005 with a mission to provide high-quality products to our customers. We pride ourselves on our commitment to excellence and customer satisfaction.</p>
+    </div>
+  );
 }
 
-export default About 
+export default AboutUs;
