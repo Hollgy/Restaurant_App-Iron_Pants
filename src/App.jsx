cb832538b9c8fc371ade4ab7689015983f18fa92
KG <karlgustav.ek@hotmail.com>
2023-04-06 09:24:46 +0200

    backdrop added. not perfected

--- src/App.jsx
+++ src/App.jsx
@@ -3,22 +3,25 @@ import Header from './components/Header.jsx'
 import Footer from './components/Footer.jsx'
 import Body from './components/Body.jsx'
 import Linkicons from './components/Linkicons.jsx'
+import Background from './components/background.jsx'
 import './App.css'
 import './components/Header.css'
 import './components/Footer.css'
 import './components/Body.css'
 import './components/Navbar.css'
 import './components/Linkicons.css'
+import './components/Background.css'
 
 function App() {
     // const [count, setCount] = useState(0)
 
     return (
-        
+
         <div className="App">
             <Header />
             <Body />
             <Linkicons />
+            <Background />
             <Footer />
 
         </div>
---
+++ src/components/Background.jsx
@@ -0,0 +1,14 @@
+import background from '/public/images/chili_backdrop.png';
+
+const Background = () => {
+
+    return (
+        <div>
+            <div>
+                <img className='background_image' src={background} alt="backdrop" />
+            </div>
+        </div>
+    )
+}
+
+export default Background
