29c82e4a5c76c995b2b0b9e89aa0a3d1453dac39
KG <karlgustav.ek@hotmail.com>
2023-04-11 13:33:08 +0200

    initial ProductForm functionality and toggle fix of menu.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -4,8 +4,9 @@ import Header from './components/Header.jsx'
 import Footer from './components/Footer.jsx'
 import Body from './components/Body.jsx'
 import Menu from './components/Menu.jsx'
-import Linkicons from './components/Linkicons'
-import Background from './components/background'
+import Linkicons from './components/Linkicons.jsx'
+import Background from './components/background.jsx'
+import ProductForm from './components/ProductForm.jsx'
 import './App.css'
 import './components/Header.css'
 import './components/Footer.css'
@@ -14,10 +15,13 @@ import './components/Navbar.css'
 import './components/Menu.css'
 import './components/Linkicons.css'
 import './components/Background.css'
+import './components/ProductForm.css'
 
 function App() {
     const [menu, setMenu] = useState(menuList)
     // menu.forEach(item => console.log(item.item))
+    const [Products, setProducts] = useState([])
+    const addProduct = Product => setProducts([...Products, Product])
 
     return (
 
@@ -25,6 +29,7 @@ function App() {
             <Header />
             <Body />
             <Menu menu={menu} setMenu={setMenu} />
+            <ProductForm />
             <Linkicons />
             <Background />
             <Footer />
--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -1,70 +1,47 @@
 import { useState } from 'react'
 
-let fillingList = []
-let ingredientsList = []
-let extraList = []
-
-const handleClick = (menu, dishId, click, setClick) => {
-	const targetDish = menu.find(dish => dishId == dish.id)
-	click ? setClick(false) : setClick(true)
-
-	fillingList = targetDish.filling
-	ingredientsList = targetDish.ingredients
-	extraList = targetDish.extra
-}
-
 function MenuItems(list) {
-
-	// console.log('inside MenuItems ' + list);
-	let jsxList = list.map(item => {
-
-		return (
-			<li key={item.name}>{item.name}</li>
-		)
-	})
-	return jsxList
+    let jsxList = list.map((item) => {
+        return <li key={item.name}>{item.name}</li>
+    })
+    return jsxList
 }
 
 const Menu = ({ menu, setMenu }) => {
+    const handleItemClick = (dishId) => {
+        setMenu((prevMenu) => {
+            return prevMenu.map((dish) => {
+                if (dish.id === dishId) {
+                    return { ...dish, expanded: !dish.expanded }
+                } else {
+                    return { ...dish, expanded: false }
+                }
+            })
+        })
+    }
 
-	// Måste lägga in egen state-variabel för varje li-element så att de inte påverkar varandra
-	const startMenu = menu.map(dish => {
-		const [click, setClick] = useState(false)
-		return (
-			< li key={dish.id} >
-				<button onClick={() => handleClick(menu, dish.id, click, setClick)}>
-					<h2>{dish.item}</h2>
-				</button>
-				<ExpandedMenu dishObject={dish} click={click} />
-			</li >
-		)
-	})
+    const menuItems = menu.map((dish) => {
+        return (
+            <li key={dish.id}>
+                <button onClick={() => handleItemClick(dish.id)}>
+                    <h2>{dish.item}</h2>
+                </button>
+                {dish.expanded && (
+                    <div>
+                        <h2>{dish.name}</h2>
+                        <h3>Fyllning</h3>
+                        <ul>{MenuItems(dish.filling)}</ul>
+                        <h3>Tillbehör</h3>
+                        <ul>{MenuItems(dish.ingredients)}</ul>
+                        <h3>Extra</h3>
+                        <ul>{MenuItems(dish.extra)}</ul>
+                    </div>
+                )}
+            </li>
+        )
+    })
 
-
-	return (
-		<ul className="start-menu">
-			{startMenu}
-		</ul>
-	)
+    return <ul className="start-menu">{menuItems}</ul>
 }
-
-const ExpandedMenu = ({ dishObject, click }) => {
-
-	if (click == true) {
-		console.log('Klickade på: ' + dishObject.item);
-		return (
-			<div>
-				<h2>{dishObject.name}</h2>
-				<h3>Fyllning</h3>
-				<ul>{MenuItems(dishObject.filling)}</ul>
-				<h3>Tillbehör</h3>
-				<ul>{MenuItems(dishObject.ingredients)}</ul>
-				<h3>Extra</h3>
-				<ul>{MenuItems(dishObject.extra)}</ul>
-			</div>
-		)
-	}
-}
-
 
 export default Menu
