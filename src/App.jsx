48108a2a8dd40f1582c20bd6fa078c8b3df8e919
KG <karlgustav.ek@hotmail.com>
2023-04-14 10:03:55 +0200

    major push or bugfixes etc, not perfect yet

--- src/App.jsx
+++ src/App.jsx
@@ -1,44 +1,56 @@
-import { useState } from 'react'
-import menuList from './assets/menuArray'
-import Header from './components/Header.jsx'
-import Footer from './components/Footer.jsx'
-import Body from './components/Body.jsx'
-import Menu from './components/Menu.jsx'
-import Linkicons from './components/Linkicons.jsx'
-import Background from './components/Background.jsx'
-import ProductForm from './components/ProductForm.jsx'
-import AboutUs from './components/About.jsx'
-import './App.css'
-import './components/Header.css'
-import './components/Footer.css'
-import './components/Body.css'
-import './components/Navbar.css'
-import './components/Menu.css'
-import './components/Linkicons.css'
-import './components/Background.css'
-import './components/about.css'
-import './components/ProductForm.css'
+import { useState, useEffect } from 'react';
+import menuList from './assets/menuArray';
+import Header from './components/Header.jsx';
+import Footer from './components/Footer.jsx';
+import Body from './components/Body.jsx';
+import Menu from './components/Menu.jsx';
+import Linkicons from './components/Linkicons.jsx';
+import Background from './components/Background.jsx';
+import ProductForm from './components/ProductForm.jsx';
+import AdditionalFood from './components/ProductSortiment.jsx';
+import AboutUs from './components/About.jsx';
+import './App.css';
+import './components/Header.css';
+import './components/Footer.css';
+import './components/Body.css';
+import './components/Navbar.css';
+import './components/Menu.css';
+import './components/Linkicons.css';
+import './components/Background.css';
+import './components/About.css';
+import './components/ProductForm.css';
+import food from './assets/Products';
+import foodRest from './assets/ProductSortiment';
 
 function App() {
-    const [menu, setMenu] = useState(menuList)
-    // menu.forEach(item => console.log(item.item))
-    const [Products, setProducts] = useState([])
-    const addProduct = Product => setProducts([...Products, Product])
+    const [menu, setMenu] = useState(menuList);
+    const [foodItems, setFoodItems] = useState([]);
+
+    useEffect(() => {
+        setFoodItems([...food, ...foodRest]);
+    }, []);
+
+    const handleAddition = (foodItem) => {
+        setFoodItems([...foodItems, foodItem]);
+    };
+
+    const handleDelete = (foodItem) => {
+        setFoodItems(foodItems.filter((item) => item.id !== foodItem.id));
+    };
 
     return (
-
         <div className="App">
             <Header />
             <Body />
             <Menu menu={menu} setMenu={setMenu} />
-            <ProductForm />
+            <ProductForm foodItems={foodItems} handleDelete={handleDelete} />
+            <AdditionalFood foodItems={foodItems} handleAddition={handleAddition} />
             <Linkicons />
             <Background />
             <AboutUs />
             <Footer />
-
         </div>
-    )
+    );
 }
 
-export default App
+export default App;
